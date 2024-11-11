f476d84443ebf40f072473959178825446114eeb
Main Dev <richkravhh@gmail.com>
2024-11-11 23:16:44 +0700

    Responsive added to Home

--- src/component/home/Intro.jsx
+++ src/component/home/Intro.jsx
@@ -2,13 +2,13 @@ import { romoIntro } from "../../utils/constants";
 
 export default function Intro () {
     return (
-        <section id="intro" className={`flex space-x-10 justify-center p-10 max-w-[50%] mx-auto`}>
-            <div id="romo" className={`bg-rblack my-auto rounded-2xl`}>
-                <img src={`/assets/romo-fuck.png`} width={1000} alt={`romo-fuck`} />
+        <section id="intro" className={`flex lg:flex-row flex-col lg:space-x-10 justify-center p-10 lg:max-w-[50%] mx-auto lg:space-y-0 space-y-4`}>
+            <div id="romo" className={`bg-rblack max-w-fit mx-auto my-auto rounded-2xl`}>
+                <img src={`/assets/romo-fuck.png`} alt={`romo-fuck`} className={`w-[150px] mx-auto`} />
             </div>
-            <div id="romo-intro" className={`my-auto`}>
-                <h1 className={`lg:text-5xl font-bold`}>{romoIntro?.title}</h1>
-                <p className="font-mali">{romoIntro?.description}</p>
+            <div id="romo-intro" className={`my-auto text-center lg:text-left`}>
+                <h1 className={`lg:text-5xl text-3xl font-bold`}>{romoIntro?.title}</h1>
+                <p className={`font-mali lg:text-md text-sm`}>{romoIntro?.description}</p>
             </div>
         </section>
     )
